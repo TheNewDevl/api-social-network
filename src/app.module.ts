9bec9d6a5c708ed0d312db4d0fa8d94ba4ca05c4
TheNewDevl <95215674+TheNewDevl@users.noreply.github.com>
2022-04-20 12:44:34 +0200

    setting DB config and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfig from './config/databaseConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: dbConfig.type,
+      host: dbConfig.host,
+      port: dbConfig.port,
+      username: dbConfig.username,
+      password: dbConfig.password,
+      database: dbConfig.name,
+      entities: [],
+      logging: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,35 @@
+const dbConfig = {
+
+    get host(): string {
+        return this.getEnvValue('DB_HOST')
+    },
+
+    get type(): "mysql" {
+        return this.getEnvValue('DB_TYPE') as "mysql"
+    },
+
+    get password(): string {
+        return this.getEnvValue('DB_PASSWORD')
+    },
+
+    get username(): string {
+        return this.getEnvValue('DB_USERNAME')
+    },
+
+    get name(): string {
+        return this.getEnvValue('DB_NAME')
+    },
+
+    get port(): number {
+        return parseInt(this.getEnvValue('DB_PORT')) || 3306
+    },
+
+    getEnvValue(key: string) {
+        const value = process.env[key]
+        if (!value) {
+            throw new Error(`You must specify ${key}. Check you .env values.`);
+        }
+        return value
+    }
+}
+export default dbConfig
